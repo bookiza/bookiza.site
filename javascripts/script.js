3f351fe26c0745347baaa666f68eabb839813cfb
Marvin Danig <marvin@marvindanig.com>
2019-04-14 11:58:16 -0400

    fetch package version dynamically. update site design a bit.

--- javascripts/script.js
+++ javascripts/script.js
@@ -40,4 +40,15 @@
       }
     }
   }
+
+
+
+  fetch('https://raw.githubusercontent.com/bookiza/bookiza.cli/master/package.json')
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(myJson) {
+      document.getElementById('version').innerHTML = `npm@${myJson.version}`
+    });
+
 })(this, document)
